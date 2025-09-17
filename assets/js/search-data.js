52ad884a8738b4353b69312bc8a37e58879ba1b9
AViSLab-PTIT <avislabptit@gmail.com>
2025-09-17 13:20:34 +0000

    Deploying to gh-pages from @ AViSLab-PTIT/avislab-ptit.github.io@d615edaf653510e24b4cab8dd0ac3e80d5c3047d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,6 +41,20 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation menu",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Materials for courses you taught. Replace this text with your description.",
+          section: "Navigation menu",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
