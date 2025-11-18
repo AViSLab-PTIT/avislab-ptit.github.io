380da7d872fa1c53a7341bcfb26d0c229d111169
AViSLab-PTIT <avislabptit@gmail.com>
2025-11-18 17:31:36 +0000

    Deploying to gh-pages from @ AViSLab-PTIT/avislab-ptit.github.io@cc77600c20dbf8d1a041d4a6248966494ce436be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -27,8 +27,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-thành-viên",
-          title: "Thành viên",
+        },{id: "nav-people",
+          title: "people",
           description: "members of the lab or group",
           section: "Navigation menu",
           handler: () => {
